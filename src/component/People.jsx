ed1063ce7a756418db08195e3c1974187c0eb4fd
naeem <naeem.tahir2810@gmail.com>
2024-03-02 17:34:40 +0500

    ading animation

--- src/component/People.jsx
+++ src/component/People.jsx
@@ -56,16 +56,16 @@ function Planets() {
       setLoading(false);
     });
   }, [urlPeople]);
-
+  //  -------------------------------------- Cards Variable -----------------------------------
   const allPlanetsOnPage = (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 justify-items-center">
       {starWarsDataPeople?.results.map((planet, index) => {
         return (
           <div
             key={planet.name}
             className={`max-w-xs rounded overflow-hidden shadow-lg ${
               backgroundColors[planet.species.length]
-            } text-gray-700`}
+            } text-gray-700 max-w-md mx-auto bg-white rounded-md overflow-hidden transform transition-transform duration-300 ease-in-out hov shadow-cyan-900 hover:scale-105 hover:shadow-md hover:shadow-amber-500 shadow-md`}
             onClick={() => openModal(planet)}
           >
             {peopleImages.length !== 0 && (
