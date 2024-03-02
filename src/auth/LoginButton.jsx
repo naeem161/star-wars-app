1d40557342b7ee499680aea2698fde50a6d32ffd
naeem <naeem.tahir2810@gmail.com>
2024-03-02 21:40:18 +0500

    change button style

--- src/auth/LoginButton.jsx
+++ src/auth/LoginButton.jsx
@@ -5,7 +5,8 @@ const LoginButton = () => {
 
   return (
     <button
-      className="btn btn-primary rounded-3xl text-3xl font-medium  bg-primaryColor p-3 mt-36  w-48 hover:shadow-lg hover:scale-105 transition-transform duration-300 ease-in-out shadow-zinc-950"
+      className="btn btn-primary   rounded-3xl text-3xl font-medium  bg-primaryColor p-3 mt-36  w-48 hover:shadow-black
+      shadow-black hover:scale-105 transition-transform duration-300 ease-in-out  text-black"
       onClick={() => loginWithRedirect()}
     >
       Log In
